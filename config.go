378aaceb92ba3ab05d1580a77d23cb643f2c9641
zhaokuiyuan <2054813048@qq.com>
2024-06-14 12:29:07 +0800

    chore: 修改etcd作为配置中心

--- config.go
+++ config.go
@@ -1,15 +1,16 @@
 package going
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"strings"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
-	"github.com/nacos-group/nacos-sdk-go/clients"
-	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	"github.com/nacos-group/nacos-sdk-go/vo"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 func InitConfig() {
@@ -27,9 +28,9 @@ func InitConfig() {
 	}
 
 	// 如果开启 nacos 配置中心，则初始化 nacos 配置，否则监听配置文件变化（不能同时使用 nacos 和监听本地文件）
-	if viper.GetBool("nacos.enable") {
+	if viper.GetBool("etcd.enable") {
 		// 初始化 nacos 配置
-		InitNacosConfig()
+		InitEtcdConfig()
 	} else {
 		// 监听配置文件变化
 		viper.WatchConfig()
@@ -39,68 +40,45 @@ func InitConfig() {
 	}
 }
 
-func InitNacosConfig() {
-	host := viper.GetString("nacos.host")
-	port := viper.GetInt("nacos.port")
-	namespace := viper.GetString("nacos.namespace")
-	dataId := viper.GetString("nacos.data_id")
-	group := viper.GetString("nacos.group")
-	username := viper.GetString("nacos.username")
-	password := viper.GetString("nacos.password")
+func InitEtcdConfig() {
+	// etcd加载配置
+	username := viper.GetString("etcd.username")
+	password := viper.GetString("etcd.password")
+	endpoints := viper.GetStringSlice("etcd.endpoints")
+	key := viper.GetString("etcd.key")
 
-	// 服务端配置（如果存在多个服务端配置，会轮询获取）
-	sc := []constant.ServerConfig{
-		*constant.NewServerConfig(host, uint64(port), constant.WithContextPath("/nacos")),
-	}
-
-	// 客户端配置
-	cc := *constant.NewClientConfig(
-		constant.WithNamespaceId(namespace),
-		constant.WithTimeoutMs(5000),
-		constant.WithNotLoadCacheAtStart(true),
-		constant.WithLogDir("/tmp/nacos/log"),
-		constant.WithCacheDir("/tmp/nacos/cache"),
-		constant.WithUsername(username),
-		constant.WithPassword(password),
-	)
-
-	// 创建客户端
-	client, err := clients.NewConfigClient(
-		vo.NacosClientParam{
-			ClientConfig:  &cc,
-			ServerConfigs: sc,
-		},
-	)
-
-	if err != nil {
-		panic(errors.WithMessage(err, "create nacos client error"))
-	}
-
-	// 获取 nacos 配置
-	content, err := client.GetConfig(vo.ConfigParam{
-		DataId: dataId,
-		Group:  group,
+	// 创建etcd客户端配置
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   endpoints, // etcd服务的地址
+		DialTimeout: 5 * time.Second,
+		Username:    username,
+		Password:    password,
 	})
 	if err != nil {
-		panic(errors.WithMessage(err, "get nacos config error"))
+		log.Fatal(err)
 	}
+	defer cli.Close()
 
-	// 写入 viper
-	viper.SetConfigType("yaml")
-	if err := viper.MergeConfig(strings.NewReader(content)); err != nil {
-		panic(errors.WithMessage(err, "viper read nacos config error"))
-	}
-
-	// 监听配置
-	err = client.ListenConfig(vo.ConfigParam{
-		DataId: dataId,
-		Group:  group,
-		OnChange: func(namespace, group, dataId, data string) {
-			// 合并入 viper
-			viper.MergeConfig(strings.NewReader(data))
-		},
-	})
+	// 读取etcd key配置
+	resp, err := cli.Get(context.Background(), key)
 	if err != nil {
-		panic(errors.WithMessage(err, "listen nacos config error"))
+		log.Fatal(err)
 	}
+
+	// 设置viper
+	for _, ev := range resp.Kvs {
+		if err := viper.MergeConfig(strings.NewReader(string(ev.Value))); err != nil {
+			panic(errors.WithMessage(err, "viper read etcd config error"))
+		}
+	}
+
+	// 监听配置变化
+	go func() {
+		rch := cli.Watch(context.Background(), key)
+		for wresp := range rch {
+			for _, ev := range wresp.Events {
+				viper.MergeConfig(strings.NewReader(string(ev.Kv.Value)))
+			}
+		}
+	}()
 }
--- go.mod
+++ go.mod
@@ -5,32 +5,28 @@ go 1.22.2
 require (
 	github.com/cloudwego/hertz v0.9.1
 	github.com/fsnotify/fsnotify v1.7.0
-	github.com/nacos-group/nacos-sdk-go v1.1.4
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/viper v1.19.0
+	go.etcd.io/etcd/client/v3 v3.5.12
 	go.uber.org/dig v1.17.1
 )
 
 require (
-	github.com/aliyun/alibaba-cloud-sdk-go v1.61.18 // indirect
-	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/bytedance/go-tagexpr/v2 v2.9.2 // indirect
 	github.com/bytedance/gopkg v0.0.0-20220413063733-65bf48ffb3a7 // indirect
 	github.com/bytedance/sonic v1.8.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/cloudwego/netpoll v0.6.0 // indirect
-	github.com/go-errors/errors v1.0.1 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/henrylee2cn/ameda v1.4.10 // indirect
 	github.com/henrylee2cn/goutil v0.0.0-20210127050712-89660552f6f8 // indirect
-	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nyaruka/phonenumbers v1.0.55 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
@@ -44,16 +40,20 @@ require (
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.12 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.12 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240311132316-a219d84964c2 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240314234333-6e1732d8331c // indirect
+	google.golang.org/grpc v1.62.1 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
