1c54b66a0f3f911ae0bd39b7b5baafe705205c1a
zhaokuiyuan <2054813048@qq.com>
2024-08-15 18:39:56 +0800

    add example

---
+++ example/cmd/demo/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/PirateDreamer/going/conf"
+	"github.com/PirateDreamer/going/ginx"
+	"github.com/PirateDreamer/going/zlog"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	conf.InitConfig()
+	zlog.InitZlog()
+	// gormx.InitMysql()
+
+	r := gin.Default()
+
+	r.GET("/ping", ginx.Run(sayHello))
+
+	r.Run()
+}
+
+type SayHelloReq struct {
+	Name string `form:"name"`
+}
+
+type SayHelloResp struct {
+	Result string `json:"result"`
+}
+
+func sayHello(ctx context.Context, c *gin.Context, param SayHelloReq) (resp *SayHelloResp, err error) {
+	zlog.Log(ctx).Info(param.Name)
+	resp = &SayHelloResp{
+		Result: fmt.Sprintf("hello %s", param.Name),
+	}
+	return
+}
--- ginx/run.go
+++ ginx/run.go
@@ -1,12 +1,14 @@
 package ginx
 
 import (
+	"context"
 	"reflect"
 	"time"
 
 	"github.com/PirateDreamer/going/comm"
 	"github.com/PirateDreamer/going/xerr"
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 )
 
 type Response struct {
@@ -20,7 +22,7 @@ type Response struct {
 
 type Empty struct{}
 
-func ResSuccess(c *gin.Context, data any) {
+func ResSuccess(ctx context.Context, c *gin.Context, data any) {
 	if data == nil {
 		data = Empty{}
 	}
@@ -29,20 +31,20 @@ func ResSuccess(c *gin.Context, data any) {
 		Code:  0,
 		Msg:   "success",
 		Time:  time.Now().Unix(),
-		ReqId: comm.GetReqId(c),
+		ReqId: comm.GetReqId(ctx),
 		Data:  data,
 	})
 }
 
-func ResFail(c *gin.Context, err error) {
-	ResFailWithData(c, err, Empty{})
+func ResFail(ctx context.Context, c *gin.Context, err error) {
+	ResFailWithData(ctx, c, err, Empty{})
 }
 
-func ResFailWithData(c *gin.Context, err error, data any) {
+func ResFailWithData(ctx context.Context, c *gin.Context, err error, data any) {
 	result := Response{
 		Time:  time.Now().Unix(),
 		Data:  data,
-		ReqId: comm.GetReqId(c),
+		ReqId: comm.GetReqId(ctx),
 	}
 	switch e := err.(type) {
 	case xerr.BizError:
@@ -57,26 +59,29 @@ func ResFailWithData(c *gin.Context, err error, data any) {
 	c.JSON(200, result)
 }
 
-func Run[T, R any](fn func(c *gin.Context, req T) (*R, error)) gin.HandlerFunc {
+func Run[T, R any](fn func(ctx context.Context, c *gin.Context, req T) (*R, error)) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx := context.WithValue(c.Request.Context(), "req_id", uuid.NewV4().String())
+
 		// 参数绑定
 		var req T
 		t := reflect.TypeOf(req)
 		if t != reflect.TypeOf(Empty{}) {
 			if err := c.Bind(&req); err != nil {
-				ResFail(c, xerr.NewCommBizErr(err.Error()))
+				ResFail(ctx, c, xerr.NewCommBizErr(err.Error()))
 				c.Abort()
 				return
 			}
 		}
+
 		// 执行处理逻辑
-		resp, err := fn(c, req)
+		resp, err := fn(ctx, c, req)
 
 		// 结果处理
 		if err != nil {
-			ResFail(c, err)
+			ResFail(ctx, c, err)
 		} else {
-			ResSuccess(c, resp)
+			ResSuccess(ctx, c, resp)
 		}
 		c.Next()
 	}
