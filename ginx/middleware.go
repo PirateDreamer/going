ff3dbabcf05e45477191fb32e3bac485e40c715a
zhaokuiyuan <2054813048@qq.com>
2025-02-10 10:30:46 +0800

    add cache middleware

--- ginx/middleware.go
+++ ginx/middleware.go
@@ -1,9 +1,17 @@
 package ginx
 
 import (
+	"bytes"
 	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"net/url"
+	"time"
 
+	"github.com/PirateDreamer/going/gredis"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func TraceMiddleware() gin.HandlerFunc {
@@ -13,3 +21,66 @@ func TraceMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+// Cache 接口缓存中间件
+func Cache(seconds, db int) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if viper.GetBool("cache.disable") {
+			c.Next()
+			return
+		}
+
+		key := "ApiCache_" + c.Request.URL.Path
+		parsedURL, err := url.Parse(c.Request.URL.String())
+		if err != nil {
+			ResFail(context.Background(), c, err)
+			return
+		}
+		// Platform
+		platform := c.Request.Header.Get("Platform")
+		// 压缩长度
+		hash := md5.New()
+		hash.Write([]byte(parsedURL.RawQuery + platform))
+		hashValue := hash.Sum(nil)
+		key += hex.EncodeToString(hashValue)
+		cacheData, err := gredis.Redis[db].Get(c, key).Result()
+		if err == nil && cacheData != "" {
+			var resData map[string]any
+			err := json.Unmarshal([]byte(cacheData), &resData)
+			if err == nil {
+				ResSuccess(context.Background(), c, resData["data"])
+				c.Abort()
+				return
+			}
+		}
+
+		// 创建一个 ResponseRecorder
+		recorder := &responseRecorder{
+			ResponseWriter: c.Writer,
+			body:           bytes.NewBufferString(""),
+		}
+		c.Writer = recorder
+
+		c.Next()
+
+		// 读取响应体
+		responseBody := recorder.body.String()
+		if responseBody != "" {
+			// 缓存
+			gredis.Redis[db].SetNX(c, key, responseBody, time.Duration(seconds)*time.Second)
+		}
+	}
+}
+
+// 自定义 ResponseRecorder，用于记录响应体
+type responseRecorder struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (r *responseRecorder) Write(b []byte) (int, error) {
+	r.body.Write(b)
+	return r.ResponseWriter.Write(b)
+}
+
+// 接口限流中间件
--- gredis/redis.go
+++ gredis/redis.go
@@ -3,23 +3,32 @@ package gredis
 import (
 	"context"
 
+	"github.com/PirateDreamer/going/zlog"
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
 )
 
-var Redis *redis.Client
+var Redis map[int]*redis.Client
 
 func InitRedis() {
-	if viper.GetString("redis.addr") == "" {
+	if viper.GetString("redis.addr") == "" && viper.GetBool("redis.disable") {
 		return
 	}
-	Redis = redis.NewClient(&redis.Options{
-		Addr:     viper.GetString("redis.addr"),
-		Password: viper.GetString("redis.pass"),
-		DB:       viper.GetInt("redis.db"),
-	})
 
-	if err := Redis.Ping(context.Background()).Err(); err != nil {
-		panic(err)
+	Redis = make(map[int]*redis.Client)
+
+	dbs := viper.GetIntSlice("redis.dbs")
+
+	for _, db := range dbs {
+		Redis[db] = redis.NewClient(&redis.Options{
+			Addr:     viper.GetString("redis.addr"),
+			Password: viper.GetString("redis.pass"),
+			Username: viper.GetString("redis.username"),
+			DB:       db,
+		})
+		if err := Redis[db].Ping(context.Background()).Err(); err != nil {
+			zlog.LogError(context.Background(), "db %d redis init error: %s", db, err.Error())
+		}
+		zlog.LogInfo(context.Background(), "db %d redis init success", db)
 	}
 }
