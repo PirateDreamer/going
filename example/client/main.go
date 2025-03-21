3ef69da65fa3dbfcaacc9fc76c566ab72c4fb584
zhaokuiyuan <2054813048@qq.com>
2025-03-21 10:43:03 +0800

    add grpc plan

--- example/client/main.go
+++ example/client/main.go
@@ -1,12 +1,29 @@
 package main
 
-import "google.golang.org/grpc"
+import (
+	userPb "client/gen/user"
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+)
 
 func main() {
-	conn, err := grpc.NewClient("0.0.0.0:8000")
+	conn, err := grpc.NewClient("0.0.0.0:50052")
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-	
+	// 创建客户端
+	client := userPb.NewUserServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	// 调用服务端逻辑
+	resp, err := client.Login(ctx, &userPb.LoginRequest{})
+	if err != nil {
+		log.Fatalf("login error: %v", err)
+	}
+	log.Printf("login resp: %s", resp.Token)
+
 }
--- example/server/main.go
+++ example/server/main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net"
+
+	userPb "server/gen/user"
+
+	"google.golang.org/grpc"
+)
+
+type UserServer struct {
+	userPb.UnimplementedUserServiceServer
+}
+
+func (s UserServer) Login(ctx context.Context, req *userPb.LoginRequest) (resp *userPb.LoginResponse, err error) {
+	return
+}
+
 func main() {
+	// 网络监听
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	// 初始化grpc服务
+	s := grpc.NewServer()
+	// 注册服务逻辑
+	userPb.RegisterUserServiceServer(s, &UserServer{})
+	// 绑定网络
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
